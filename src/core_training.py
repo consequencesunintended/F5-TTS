a19cd6ea24e408caee4bd0af634cb0823b8abf0f
consequencesunintended <44167267+consequencesunintended@users.noreply.github.com>
2025-02-18 02:32:05 +0000

    auth removed

--- src/core_training.py
+++ src/core_training.py
@@ -67,7 +67,7 @@ class HFDataset(IterableDataset):
             yield {"mel_spec": mel_spec, "text": text}
 
 
-def train(token):
+def train():
     print("Current working directory:", os.getcwd())
     wandb.init(project="emilia")
 
@@ -77,7 +77,6 @@ def train(token):
             "amphion/Emilia-Dataset",
             data_dir="Emilia/EN",
             split="train",
-            use_auth_token=token,
             streaming=True,
         ).with_format("torch")
     )
